919f278268851ae07b97d43409c971f432055759
JoshuaBentley <joshuabentley911@gmail.com>
2023-04-05 18:26:12 -0700

    creadit css for nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { useState, useEffect }  from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap/';
 // import logos and icons from assests folder i will set up later 
 
 export const NavBar = () => {
